3c222be6d39cc9471b10ea6940cd5531dda13022
vitorarevalo <vitinhoosuna2008@gmail.com>
2024-02-10 16:23:17 -0300

    aprendendo propriedades do document

---
+++ CURSO MATHEUS BATTISTI/EXERCICIOS/ex042/js/script.js
@@ -0,0 +1,14 @@
+// Propriedades Document
+console.log(document.body)
+
+console.log(document.head)
+
+console.log(document.links[0])
+
+document.links[0].textContent = "Twitter"
+
+console.log(document.URL);
+
+console.group(document.title);
+
+document.title = "Testando"
