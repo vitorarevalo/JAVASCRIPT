f4aec664b1fbdbc8f3a1bec7ba039d1db058c90f
vitorarevalo <vitinhoosuna2008@gmail.com>
2024-02-24 21:31:09 -0300

    vou começar o projeto de javascript

---
+++ CURSO MATHEUS BATTISTI/EXERCICIOS/ex049/js/script.js
@@ -0,0 +1,13 @@
+// keydown
+document.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        console.log("Apertou Enter")
+    }    
+})
+
+// keyup
+document.addEventListener("keyup", function(e){
+    if(e.key === "Enter"){
+        console.log("Soltou o Enter")
+    }
+})
